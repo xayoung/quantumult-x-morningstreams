bdb512f962295cf1100d85149e676fca3bfdbecc
xayoung <xayoung@hotmail.com>
2022-05-18 18:51:25 +0800

    Update response.js

--- response.js
+++ response.js
@@ -11,14 +11,10 @@
 // $response.statusCode, $response.headers
 
 var modifiedHeaders = $response.headers;
-if ($request.url.indexOf('ott-video-cf.formula1.com') || $request.url.indexOf('ott-video-fer-cf.formula1.com') || $request.url.indexOf('f1tv.formula1.com')) {
-  modifiedHeaders['*'] = 'Access-Control-Allow-Origin';
-  modifiedHeaders['true'] = 'Access-Control-Allow-Credentials';
-  modifiedHeaders['GET, PUT, POST, DELETE, HEAD, OPTIONS'] = 'Access-Control-Allow-Methods';
-  $done({headers : modifiedHeaders});
-} else {
-  $done({});
-}
+modifiedHeaders['*'] = 'Access-Control-Allow-Origin';
+modifiedHeaders['true'] = 'Access-Control-Allow-Credentials';
+modifiedHeaders['GET, PUT, POST, DELETE, HEAD, OPTIONS'] = 'Access-Control-Allow-Methods';
+$done({headers : modifiedHeaders});
 
 // $done({headers : modifiedHeaders});
 // $done({}); // Not changed.
