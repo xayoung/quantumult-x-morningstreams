617eb9067558b2a67b884ddeec2685de41b6fda5
xayoung <xayoung@hotmail.com>
2022-05-18 17:56:32 +0800

    remove some code.

--- request.js
+++ request.js
@@ -1,15 +1,3 @@
-/**
- * @fileoverview Example of HTTP rewrite of response header.
- *
- * @supported Quantumult X (v1.0.5-build183)
- *
- * [rewrite_local]
- * ^http://example\.com/resource8/ url script-response-header response-header.js
- */
-
-// $request.scheme, $request.method, $request.url, $request.path, $request.headers
-// $response.statusCode, $response.headers
-
 var modifiedHeaders = $request.headers;
 var url = $request.url;
 if (url.indexOf('ott-video-cf.formula1.com') || url.indexOf('ott-video-fer-cf.formula1.com') || url.indexOf('f1tv.formula1.com')) {
@@ -20,6 +8,3 @@ if (url.indexOf('ott-video-cf.formula1.com') || url.indexOf('ott-video-fer-cf.fo
 } else {
   $done({});
 }
-
-// $done({headers : modifiedHeaders});
-// $done({}); // Not changed.
