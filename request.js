fd6376e31337a0fb19922d0c9c4931e8f5d03f58
xayoung <xayoung@hotmail.com>
2022-05-18 18:46:15 +0800

    Update request.js

--- request.js
+++ request.js
@@ -1,10 +1,5 @@
 var modifiedHeaders = $request.headers;
-var url = $request.url;
-if (url.indexOf('ott-video-cf.formula1.com') || url.indexOf('ott-video-fer-cf.formula1.com') || url.indexOf('f1tv.formula1.com')) {
-  modifiedHeaders['undefined=true'] = 'Cookie';
-  modifiedHeaders['https://f1tv.formula1.com/'] = 'Origin';
-  modifiedHeaders['https://f1tv.formula1.com/detail/1000005114/2022-saudi-arabian-grand-prix?action=play'] = 'Referer';
-  $done({headers : modifiedHeaders});
-} else {
-  $done({});
-}
+modifiedHeaders['undefined=true'] = 'Cookie';
+modifiedHeaders['https://f1tv.formula1.com/'] = 'Origin';
+modifiedHeaders['https://f1tv.formula1.com/detail/1000005114/2022-saudi-arabian-grand-prix?action=play'] = 'Referer';
+$done({headers : modifiedHeaders});
