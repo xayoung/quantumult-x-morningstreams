27e5ca444347db23efbc21355e02ffba3379f4e8
xayoung <xayoung@hotmail.com>
2022-05-19 09:15:11 +0800

    Update request-s.js

--- request-s.js
+++ request-s.js
@@ -2,7 +2,7 @@ var modifiedHeaders = $request.headers;
 var cookie = $prefs.valueForKey("f1msck");
 const isHaveCookie = typeof cookie != "undefined";
 if (isHaveCookie) {
-  modifiedHeaders['Set-Cookie'] = cookie.split(';')[0];
+  modifiedHeaders['cookie'] = cookie.split(';')[0];
 }
 modifiedHeaders['Origin'] = 'https://f1tv.formula1.com/';
 modifiedHeaders['Referer'] = 'https://f1tv.formula1.com/detail/1000005114/2022-saudi-arabian-grand-prix?action=play';
