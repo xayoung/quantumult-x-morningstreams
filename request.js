ed4c3877e3b10f298c7b251b6630ea5bb7cb7529
xayoung <xayoung@hotmail.com>
2022-09-06 09:54:29 +0800

    Update request.js

--- request.js
+++ request.js
@@ -1,7 +1,8 @@
 var modifiedHeaders = $request.headers;
 $prefs.setValueForKey('undefined', "f1msck");
-var scheme = $request.scheme
-modifiedHeaders['rscheme'] = scheme;
+var path = $request.path;
+var token = path.split('&ascendontoken=')[1];
+modifiedHeaders['ascendontoken'] = token;
 modifiedHeaders['Cookie'] = 'undefined=true';
 modifiedHeaders['Origin'] = 'https://f1tv.formula1.com/';
 modifiedHeaders['Referer'] = 'https://f1tv.formula1.com/detail/1000005114/2022-saudi-arabian-grand-prix?action=play';
